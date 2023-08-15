8bdfc6d412c24a618256e910def2b66ed89a4f7a
= <eem.mesud.28@gmail.com>
2023-08-16 02:21:56 +0300

    Mesud_S7_Challenge_Pizzaa

--- src/components/order.js
+++ src/components/order.js
@@ -1,37 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Link,useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import RadioBtn from "./radioBtn";
-import axios from 'axios';
+import axios from "axios";
 import "./../style/order.css";
 
-
 const Order = (props) => {
-  const {propsdata,setPostResData,setArray} = props;
-  const { name, price, info, puan, numb} =  propsdata;
+  const { propsdata, setPostResData, setArray } = props;
+  const { name, price, info, puan, numb } = propsdata;
   const [orderFormData, setOrderFormData] = useState({
+    // id: '',
     orderName: `${name}`,
-    default_price : numb,
+    default_price: numb,
     personName: "",
     boyutInfo: "",
     hamurTipi: "",
     ekMalzemeler: [],
     order_Note: "",
     order_Count: "",
-
   });
+
   const [siparisBtn, setSiparisBtn] = useState(false);
   const [secenekBtn, setSecenekBtn] = useState(false);
   const [mainPageBtn, setMainPgeBtn] = useState(false);
   const [orderCount, setOrderCount] = useState(1);
-  const [radioState, setRadioState] = useState('');
+  const [radioState, setRadioState] = useState("");
 
-  const [orderNote, setOrderNote] = useState('');
-  const [pername, setPerName] = useState('');
+  const [orderNote, setOrderNote] = useState("");
+  const [pername, setPerName] = useState("");
 
-  const [check,setCheck] = useState([]);
-  const [select, setSelect ] = useState('- Hamur Kalınlığı Seç -')
-
-  
+  const [check, setCheck] = useState([]);
+  const [select, setSelect] = useState("- Hamur Kalınlığı Seç -");
 
   const mainPage = useHistory("/");
 
@@ -68,96 +66,116 @@ const Order = (props) => {
   };
 
   const handleUpdateForm = () => {
-    // (Math.random()*1000+500).toString(); || Date.now() ile form id verilebilir ancak post responce zaten id döndürüyorr !!! 
+    // (Math.random()*1000+500).toString(); || Date.now() ile form id verilebilir ancak post responce zaten id döndürüyorr !!!
     orderFormData.hamurTipi = select;
     orderFormData.boyutInfo = radioState;
     orderFormData.personName = pername;
     orderFormData.order_Count = orderCount;
     orderFormData.order_Note = orderNote;
     orderFormData.ekMalzemeler = check;
+    // orderFormData.id = Date.now().toString();
     return orderFormData;
-  }
+  };
 
- const handleCheckControl = () => {
-   let checkControl;
-   const notAgain = [];
-   if([...orderFormData.ekMalzemeler].length>0){
-     checkControl = [...orderFormData.ekMalzemeler].reduce((acc,content) => {
-       if(!(content in acc)){
-         acc[content] = 1;
-       }
-       else {
-         acc[content]++;
-       }
-       return acc;
-     },{})
-   }
-   else 
-   {return null;} 
-   for (let item in checkControl) {
-     !(checkControl[item] %2 === 0) &&  notAgain.push(item);
-   }
-   setCheck(notAgain)
- }
+  const handleCheckControl = () => {
+    let checkControl;
+    const notAgain = [];
+    if ([...orderFormData.ekMalzemeler].length > 0) {
+      checkControl = [...orderFormData.ekMalzemeler].reduce((acc, content) => {
+        if (!(content in acc)) {
+          acc[content] = 1;
+        } else {
+          acc[content]++;
+        }
+        return acc;
+      }, {});
+    } else {
+      return null;
+    }
+    for (let item in checkControl) {
+      !(checkControl[item] % 2 === 0) && notAgain.push(item);
+    }
+    setCheck(notAgain);
+  };
 
-
-  const orderCountArttir = (e) => { 
+  const orderCountArttir = (e) => {
     setOrderCount(orderCount + 1);
   };
   const orderCountAzalt = (e) => {
     orderCount > 1 && setOrderCount(orderCount - 1);
   };
 
-const calculatePrice = () => {
-   let price = numb;
-   if (check.length>0){
-      price += (check.length)*5;
+  const calculatePrice = () => {
+    let price = numb;
+    if (check.length > 0) {
+      price += check.length * 5;
       price *= orderCount;
       return price;
-    }
-    else return price*(orderCount);     
-  }
-    
+    } else return price * orderCount;
+  };
 
-const handleSubmitForm = (e) =>{ 
-    
+  const handleSubmitForm = (e) => {
     axios
-    .post('https://reqres.in/api/users',orderFormData)
-    .then((res) => {
-      console.log('post res-data: ',res.data)
-      setPostResData(res.data);
+      .post("https://reqres.in/api/users", orderFormData)
+      .then((res) => {
+        console.log("post res-data: ", res.data);
+        setPostResData(res.data);
+      })
+      .catch((error) => {
+        const docBody = document.querySelector("body");
+        docBody.style.cssText = "text-align:center;padding:10px";
+        document.body.textContent = "Sunucu Hatası - 404";
+      });
+  };
 
-    })
-    .catch((error) => console.log(error));
-}
+  //#################################### FETCH İLE BASKA BİR MOCK APİ POST DENEMESİ ##########################################//
+  // fetch('https://jsonplaceholder.typicode.com/posts', {
+  //   method: 'POST',
+  //   body: JSON.stringify({...orderFormData}),
+  //   headers: {
+  //     'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     console.log(json)
+  //     setPostResData(json);
+  //   });
+  //############################################################################################################################//
 
-
-const submitStatus = () => {
-  return (!(select === '- Hamur Kalınlığı Seç -') && radioState.length>0 && pername.length>4 && orderCount<11 && (orderNote.length<50 && orderNote.length>0) && !(name === undefined) && check.length>0)
-}
-
+  const submitStatus = () => {
+    return (
+      !(select === "- Hamur Kalınlığı Seç -") &&
+      radioState.length > 0 &&
+      pername.length > 4 &&
+      orderCount < 11 &&
+      orderNote.length < 50 &&
+      orderNote.length > 0 &&
+      !(name === undefined) &&
+      check.length > 0
+    );
+  };
 
   useEffect(() => {
     handleCheckControl();
-   },[orderFormData])
+  }, [orderFormData]);
 
   useEffect(() => {
-     console.log("Boyut : ", radioState);
-   }, [radioState]);
+    console.log("Boyut : ", radioState);
+  }, [radioState]);
 
   useEffect(() => {
-     console.log("Form_Data : ", handleUpdateForm());
-     setArray(orderFormData.ekMalzemeler);
-   },[orderCount,orderNote,radioState,select,check]);
+    console.log("Form_Data : ", handleUpdateForm());
+    setArray(orderFormData.ekMalzemeler);
+  }, [orderCount, orderNote, radioState, select, check]);
 
-   useEffect(() => {
+  useEffect(() => {
     console.log("order-count : ", orderCount);
-  },[orderCount]);
+  }, [orderCount]);
 
   useEffect(() => {
     calculatePrice();
-  },[orderCount,check,orderFormData]);
-
+  }, [orderCount, check, orderFormData]);
 
   return (
     <>
@@ -222,17 +240,34 @@ const submitStatus = () => {
           <form>
             <div className="form-head">
               <div className="form-boyut">
-                <RadioBtn setRadioState = {setRadioState} radioState={radioState} />
+                <RadioBtn
+                  setRadioState={setRadioState}
+                  radioState={radioState}
+                />
               </div>
               <div className="form-hamur">
-                <h3 style={{color: (select === '- Hamur Kalınlığı Seç -') && '#ce2829' }}>
+                <h3
+                  style={{
+                    color: select === "- Hamur Kalınlığı Seç -" && "#ce2829",
+                  }}
+                >
                   Hamur Seç{" "}
                   <span style={{ color: "#ce2829", paddingLeft: "5px" }}>
                     *
                   </span>
                 </h3>
-                <select className="select" value={select} id="hamur-type" name = 'hamurTipi' onChange={(e) => {setSelect(e.target.value)}}>
-                  <option value= "- Hamur Kalınlığı Seç -" disabled>- Hamur Kalınlığı Seç -</option>
+                <select
+                  className="select"
+                  value={select}
+                  id="hamur-type"
+                  name="hamurTipi"
+                  onChange={(e) => {
+                    setSelect(e.target.value);
+                  }}
+                >
+                  <option value="- Hamur Kalınlığı Seç -" disabled>
+                    - Hamur Kalınlığı Seç -
+                  </option>
                   <option value="ince-Hamur">İnce Hamur</option>
                   <option value="Normal-Hamur">Normal Hamur</option>
                   <option value="incecik-Hamur">İncecik Hamur</option>
@@ -242,7 +277,13 @@ const submitStatus = () => {
             </div>
             <div className="choise">
               <h3>Ek Malzemeler</h3>
-              {check.length > 10 ? <p style={{color:'#ce2829'}}>Daha az malzeme seciniz ( Max 10 ) !</p>  :<p>En Fazla 10 malzeme secçebilirsiniz.5₺</p>}
+              {check.length > 10 ? (
+                <p style={{ color: "#ce2829" }}>
+                  Daha az malzeme seciniz ( Max 10 ) !
+                </p>
+              ) : (
+                <p>En Fazla 10 malzeme secçebilirsiniz.5₺</p>
+              )}
               <div className="checkbox-section">
                 {checkValues.map((item, index) => {
                   return (
@@ -259,46 +300,80 @@ const submitStatus = () => {
                           });
                         }}
                       />
-                      <label key={Date.now()} htmlFor={index}>{item}</label>
+                      <label key={Date.now()} htmlFor={index}>
+                        {item}
+                      </label>
                     </>
                   );
                 })}
               </div>
             </div>
             <div className="form-bottom">
-              <input className="not-input" placeholder="Adınız ..." style={{marginTop:'10px'}} onChange={(e) => {setPerName(e.target.value)}}></input>
-              {pername.length < 5 &&  <span style={{color: '#ce2829'}}>İsim en az 5 karakter olmalı !</span>}
+              <input
+                className="not-input"
+                placeholder="Adınız ..."
+                style={{ marginTop: "10px" }}
+                onChange={(e) => {
+                  setPerName(e.target.value);
+                }}
+              ></input>
+              {pername.length < 5 && (
+                <span style={{ color: "#ce2829" }}>
+                  İsim en az 5 karakter olmalı !
+                </span>
+              )}
               <h3 className="not">Sipariş Notu</h3>
               <input
                 className="not-input"
                 name="order_Note"
                 type="text"
                 placeholder="Siparişine eklemek istediğin bir not var mı?"
-                onChange={(e) => {setOrderNote(e.target.value)}}
-              /> 
-              {orderNote.length >= 50 && <h3 style={{color:'#ce2829'}}>Max 50 Karakter Giriniz !</h3>}
+                onChange={(e) => {
+                  setOrderNote(e.target.value);
+                }}
+              />
+              {orderNote.length >= 50 && (
+                <h3 style={{ color: "#ce2829" }}>Max 50 Karakter Giriniz !</h3>
+              )}
               <hr style={{ color: "#1a1a1a", opacity: "0.6" }} />
               <div className="total-div">
                 <div className="counter-div">
-                  <span style={{cursor:'default'}} onClick={orderCountAzalt}>-</span>
+                  <span style={{ cursor: "default" }} onClick={orderCountAzalt}>
+                    -
+                  </span>
                   <span>{orderCount}</span>
-                  <span style={{cursor:'default'}} onClick={orderCountArttir}>+</span>
+                  <span
+                    style={{ cursor: "default" }}
+                    onClick={orderCountArttir}
+                  >
+                    +
+                  </span>
                 </div>
                 <div className="form-calculate">
                   <div className="calculate-top">
                     <h3>Sipariş Toplamı</h3>
                     <div className="secim">
                       <span className="secim-span">Seçimler</span>
-                      <span className="secim-span">{((check.length)*5*(orderCount)).toString()+'₺'}</span>
+                      <span className="secim-span">
+                        {(check.length * 5 * orderCount).toString() + "₺"}
+                      </span>
                     </div>
                     <div className="total">
                       <span className="total-spam">Toplam</span>
-                      <span className="total-spam">{calculatePrice().toString()+'₺'}</span>
+                      <span className="total-spam">
+                        {calculatePrice().toString() + "₺"}
+                      </span>
                     </div>
                   </div>
-                   <Link to="/order/onay"> 
-                    <button disabled = {(submitStatus()) ? false : true} type="submit" onClick={handleSubmitForm}>SİPARİŞ VER</button>
-                   </Link>  
+                  <Link to="/order/onay">
+                    <button
+                      disabled={submitStatus() ? false : true}
+                      type="submit"
+                      onClick={handleSubmitForm}
+                    >
+                      SİPARİŞ VER
+                    </button>
+                  </Link>
                 </div>
               </div>
             </div>
